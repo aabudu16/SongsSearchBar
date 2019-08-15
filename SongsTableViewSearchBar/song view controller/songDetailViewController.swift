a5e170d65e4e813395e35103332180abfee29692
Mr Wonderful <mrwonderful@Mrs-MacBook-Pro.local>
2019-08-15 16:30:10 -0400

    Adds an imageView and 2 labels to the detailed view controller. Sorts all files into folders

---
+++ SongsTableViewSearchBar/song view controller/songDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  songDetailViewController.swift
+//  SongsTableViewSearchBar
+//
+//  Created by Mr Wonderful on 8/15/19.
+//  Copyright © 2019 C4Q . All rights reserved.
+//
+
+import UIKit
+
+class songDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
