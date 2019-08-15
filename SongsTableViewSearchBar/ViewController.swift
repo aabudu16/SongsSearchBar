059f7727f6f9fc8d40894163de13a9e3717948cc
Mr Wonderful <mrwonderful@Mrs-MacBook-Pro.local>
2019-08-15 16:13:51 -0400

    Adds code to the cellForRowAt to display songs and artists

--- SongsTableViewSearchBar/ViewController.swift
+++ SongsTableViewSearchBar/ViewController.swift
@@ -20,13 +20,21 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "songCell"){
+       let info = song[indexPath.row]
+        cell.textLabel?.text = info.name
+        cell.detailTextLabel?.text = info.artist
+        
+        return cell
     }
-    
+       return UITableViewCell()
+    }
 
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        songTextView.delegate = self
+        songTextView.dataSource = self
         // Do any additional setup after loading the view, typically from a nib.
         
     }
